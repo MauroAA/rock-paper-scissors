7d8a9d9c613fe7b5204545de1bd10decedadccb4
mauroprograma10 <programadorcerodiez@gmail.com>
2023-09-04 18:51:53 -0300

    game running

--- javaScript.js
+++ javaScript.js
@@ -1,17 +1,26 @@
 let rockPaperScissors = ["Rock", "Paper", "Scissors"];
-let playerSelection = prompt ("Choise your wapon");
-const computerSelection = getComputerChoice ();
+let recordPlayer = 0;
+let recordMachine = 0;
 
-function getComputerChoice () {
+
+//Funcion para generar una opcion Random.
+function getComputerChoice () { //Esto todavia no se ejecuta porque no la llamamos.
     let randomRockPaperScissors = Math.floor (Math.random () * rockPaperScissors.length);
     return rockPaperScissors [randomRockPaperScissors];
 }
 
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Scissors" || playerSelection === "Scissors" && computerSelection === "Rock") {
+//Funcion para generar el juego
+function playRound (playerSelection, computerSelection) { //Esto todavia no se ejecuta porque no la llamamos.
+    if (playerSelection === "Rock" && computerSelection === "Paper" || 
+    playerSelection === "Paper" && computerSelection === "Scissors" ||  //The chances of the machine winning 
+    playerSelection === "Scissors" && computerSelection === "Rock") {
+        recordMachine++
         return "Machine WON";
     }
-    else if (playerSelection === "Paper" && computerSelection === "Rock" || playerSelection === "Scissors" && computerSelection === "Paper" || playerSelection === "Rock" && computerSelection === "Scissors") {
+    else if (playerSelection === "Paper" && computerSelection === "Rock" || 
+    playerSelection === "Scissors" && computerSelection === "Paper" || //The chances of the player winning
+    playerSelection === "Rock" && computerSelection === "Scissors") {
+        recordPlayer++
         return "You Won"
     }
     else {
@@ -19,9 +28,30 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
+//Funcion para llamar el juego 5 veces
+function game () {
+    for (let i = 1; i <= 5; i++) {
+        var playerSelection = prompt ("Choise your wapon");
+        playerSelection.toLowerCase ();
+        const computerSelection = getComputerChoice (); //La guardamos en una variable y llamamos.
+        computerSelection.toLowerCase ();
+        console.log (playRound (playerSelection, computerSelection) + "\n"); //Aqui la llamamos, aqui se ejecuta.
+    }
+    if (recordPlayer > recordMachine) {
+        console.log ("Player WON the GAME!");
+    } 
+    else if (recordPlayer === recordMachine) {
+        console.log ("TIE GAME");
+    }
+    else {
+        console.log ("Game over, Machine WON the GAME");
+    }
+}
 
 
-console.log (playRound (playerSelection, computerSelection));
+
+game ();
+
 
 
 
