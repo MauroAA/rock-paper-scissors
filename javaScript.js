44c6b4c17a7d7d12a337a81d2c951ea22b01ef29
mauroprograma10 <programadorcerodiez@gmail.com>
2023-09-10 15:42:45 -0300

    this one improve too

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,45 @@
 let rockPaperScissors = ["Rock", "Paper", "Scissors"];
+let finalRecord = document.createElement ('div');
+let restarGame = document.createElement ('a');
+finalRecord.classList.add ('finalRecord');
+let container = document.querySelector ('.container');
+container.appendChild (finalRecord);
 let recordPlayer = 0;
 let recordMachine = 0;
+
+const winner = document.querySelector ('.announceWinner');
+const btnRock = document.getElementById ('rock');
+const btnPaper = document.getElementById ('paper');
+const btnScissors = document.getElementById ('scissors');
+const computerSelection = '';
+const buttons = document.querySelectorAll ('.button');
+const player = document.getElementById ('player');
+const machine = document.getElementById ('machine');
+
+var playerSelection = '';
+
+
+
+btnRock.addEventListener ('click', () => {
+    btnRock.classList.add ('pressButton');
+    playerSelection = 'Rock';
+    winner.style.display = 'block';
+    game ();
+})
+
+btnScissors.addEventListener ('click', () => {
+    btnScissors.classList.add ('pressButton');
+    playerSelection = 'Scissors';
+    winner.style.display = 'block';
+    game ();
+})
+
+btnPaper.addEventListener ('click', () => {
+    btnPaper.classList.add ('pressButton');
+    playerSelection = 'Paper';
+    winner.style.display = 'block';
+    game ();
+})
 
 
 //Funcion para generar una opcion Random.
@@ -9,51 +48,50 @@ function getComputerChoice () { //Esto todavia no se ejecuta porque no la llamam
     return rockPaperScissors [randomRockPaperScissors];
 }
 
+
 //Funcion para generar el juego
 function playRound (playerSelection, computerSelection) { //Esto todavia no se ejecuta porque no la llamamos.
     if (playerSelection === "Rock" && computerSelection === "Paper" || 
     playerSelection === "Paper" && computerSelection === "Scissors" ||  //The chances of the machine winning 
     playerSelection === "Scissors" && computerSelection === "Rock") {
         recordMachine++
-        return "Machine WON";
+        winner.innerHTML = 'Machine WON';
+        return;
     }
     else if (playerSelection === "Paper" && computerSelection === "Rock" || 
     playerSelection === "Scissors" && computerSelection === "Paper" || //The chances of the player winning
     playerSelection === "Rock" && computerSelection === "Scissors") {
         recordPlayer++
-        return "You Won"
+        winner.innerHTML = 'Player WON';
+        return;
     }
     else {
-        return "is a TIE";
+        winner.innerHTML = 'is a TIE';
     }
 }
 
-//Funcion para llamar el juego 5 veces
 function game () {
-    for (let i = 1; i <= 5; i++) {
-        var playerSelection = prompt ("Choise your wapon");
-        playerSelection.toLowerCase ();
-        const computerSelection = getComputerChoice (); //La guardamos en una variable y llamamos.
-        computerSelection.toLowerCase ();
-        console.log (playRound (playerSelection, computerSelection) + "\n"); //Aqui la llamamos, aqui se ejecuta.
-    }
-    if (recordPlayer > recordMachine) {
-        console.log ("Player WON the GAME!");
-    } 
-    else if (recordPlayer === recordMachine) {
-        console.log ("TIE GAME");
-    }
-    else {
-        console.log ("Game over, Machine WON the GAME");
+     const computerSelection = getComputerChoice ();
+     playRound (playerSelection, computerSelection);
+     player.textContent = `Player: ${recordPlayer}`;
+     machine.textContent = `Machine: ${recordMachine}`;
+
+    if (recordPlayer === 5) {
+            finalRecord.innerHTML = 'Player wins the match'
+
+    } else if (recordMachine === 5) {
+            finalRecord.innerHTML = 'Machine wins the match'
     }
 }
 
 
+function removeTransition (e) {
+    if (e.propertyName === 'transform') return;
+    this.classList.remove ('pressButton');
+}
 
-game ();
 
-
-
+buttons.forEach ((btn) => btn.addEventListener ("transitionend", removeTransition));
 
 
 
